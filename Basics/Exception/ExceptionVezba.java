ade62dbfdb54c0eab89abc37205fb52dc5da16ca
QA1.5 <aleksa.v@yahoo.com>
2023-05-18 20:35:30 +0200

    Predavanja 8.

--- Basics/Exception/ExceptionVezba.java
+++ Basics/Exception/ExceptionVezba.java
@@ -1,6 +1,9 @@
 package Exception;
 import java.io.IOException;
 public class ExceptionVezba {
+
+    //Write a Java program that takes a number as input and prints itsmultiplication table up to 10
+
     public static void main(String[] args) {
         try {
             int c = 30/0;
---
+++ Basics/Exception/Vezba2.java
@@ -0,0 +1,20 @@
+package Exception;
+
+import java.util.Scanner;
+
+public class Vezba2 {
+
+    // Write a Java program that takes a number as input and prints itsmultiplication table up to 10
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        int num1 = in.nextInt();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(num1 + " x " + (i + 1) + " = " + (num1 * (i + 1)));
+        }
+    }
+}
